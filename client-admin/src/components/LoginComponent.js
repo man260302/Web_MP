0e51236b5f84678a78f8f418ccf36c3b80f7f40c
man260302 <103521906+man260302@users.noreply.github.com>
2023-07-21 22:34:56 +0700

    Update LoginComponent.js

--- client-admin/src/components/LoginComponent.js
+++ client-admin/src/components/LoginComponent.js
@@ -7,8 +7,8 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      txtUsername: 'admin',
-      txtPassword: '12345'
+      txtUsername: '',
+      txtPassword: ''
     };
   }
   render() {
